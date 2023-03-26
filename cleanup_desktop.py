cf1222deba65d963e06b7d3de14e5bdfc8eb691a
Teomen77 <49922991+Teomen77@users.noreply.github.com>
2023-03-26 22:28:41 +0200

    Update cleanup_desktop.py

--- cleanup_desktop.py
+++ cleanup_desktop.py
@@ -16,8 +16,7 @@ if not os.path.exists(folder_path):
 #looping through every element in desktop_path
 for filename in os.listdir(desktop_path):
     #only taking elements wit the specific endings
-    if filename.endswith((".pdf",".txt", ".docx", ".png", ".jpg", ".docx", ".odt", ".ods")):
-        print(f"Moving file: {filename}")
+    if filename.endswith((".pdf",".txt", ".docx", ".png", ".jpg", ".docx", ".odt", ".ods"))
         file_path = os.path.join(desktop_path, filename)
         destination_path = os.path.join(folder_path, filename)
         shutil.move(file_path, destination_path)
